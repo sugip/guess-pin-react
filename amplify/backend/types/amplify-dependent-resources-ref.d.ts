c9a5d394de56f6808f7062465d92998df699730d
takahiko-sugihara <takahiko-sugihara@sugihara-work.local>
2022-03-28 15:38:46 +0900

    feat: add auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "guesspinreactab3e4f90": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,23 @@ import { useReducer } from 'react';
 import AnswerList from './components/AnswerList';
 import NumPad from './components/NumPad';
 import { reducer, initialState } from './gameReducer';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
-    <div className='guess-pin'>
-      <AnswerList pinList={state.pinList} />
-      <NumPad dispatch={dispatch} />
-      {state.message && <div className='message'>{state.message}</div>}
-    </div>
+    <Authenticator>
+      {({ signOut }) => (
+        <div className='guess-pin'>
+          <AnswerList pinList={state.pinList} />
+          <NumPad dispatch={dispatch} />
+          <button onClick={signOut}>sign out</button>
+          {state.message && <div className='message'>{state.message}</div>}
+        </div>
+      )}
+    </Authenticator>
   );
 };
 
