50bff4382ae35447472148eb18fd06a37adce023
sugip <sugip952@gmail.com>
2023-06-10 21:11:18 +0900

    feat: remove amplify related code

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,16 @@ import { useReducer } from 'react';
 import AnswerList from './components/AnswerList';
 import NumPad from './components/NumPad';
 import { reducer, initialState } from './gameReducer';
-import { Authenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
-    <Authenticator>
-      {({ signOut }) => (
-        <div className='guess-pin'>
-          <AnswerList pinList={state.pinList} />
-          <NumPad dispatch={dispatch} />
-          <button onClick={signOut}>sign out</button>
-          {state.message && <div className='message'>{state.message}</div>}
-        </div>
-      )}
-    </Authenticator>
+    <div className='guess-pin'>
+      <AnswerList pinList={state.pinList} />
+      <NumPad dispatch={dispatch} />
+      {state.message && <div className='message'>{state.message}</div>}
+    </div>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports';
-
-Amplify.configure(awsExports);
 
 ReactDOM.render(
   <React.StrictMode>
